85640c9eebeb5b0013dbdc122b0db39c89b8a36a
Arnold Kell <arnold@kell.sh>
2022-10-12 20:00:28 +0300

    fix some issues with lunarvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -14,12 +14,12 @@ lvim.keys.normal_mode["<Esc>"] = ":nohlsearch<CR>"
 lvim.keys.normal_mode["<C-p>"] = ":Telescope git_files<CR>"
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
--- lvim.builtin.alpha.active = true
--- lvim.builtin.alpha.mode = "dashboard"
--- lvim.builtin.notify.active = true
--- lvim.builtin.terminal.active = true
--- lvim.builtin.nvimtree.setup.view.side = "left"
--- lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -65,10 +65,17 @@ lvim.plugins = {
 }
 
 -- Debugger
-lvim.builtin.dap = true
+-- lvim.builtin.dap = true
 
 -- Relative line numbers
 vim.opt.relativenumber = true
 
 -- Disable mouse
 vim.opt.mouse = ""
+
+-- Disable moving lines with alt j and k (Messes with my tmux config)
+-- The line swapping still works like that in visual mode, because it's kinda useful
+lvim.keys.insert_mode["<A-j>"] = false
+lvim.keys.insert_mode["<A-k>"] = false
+lvim.keys.normal_mode["<A-j>"] = false
+lvim.keys.normal_mode["<A-k>"] = false
